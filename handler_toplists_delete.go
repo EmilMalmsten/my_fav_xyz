103bd6978810c55d83afdf434e41d77594b993d4
Emil Malmsten <emil_malmsten@hotmail.com>
2023-06-17 14:14:37 +0700

    check so userID in req matches toplist owner for deletion

--- handler_toplists_delete.go
+++ handler_toplists_delete.go
@@ -1,19 +1,45 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"strconv"
 
+	"github.com/emilmalmsten/my_top_xyz/internal/database"
 	"github.com/go-chi/chi"
 )
 
 func (cfg *apiConfig) handlerToplistsDelete(w http.ResponseWriter, r *http.Request) {
 	toplistIDString := chi.URLParam(r, "toplistID")
+
 	toplistID, err := strconv.Atoi(toplistIDString)
 	if err != nil {
 		fmt.Println(err)
 		respondWithError(w, http.StatusBadRequest, "Invalid toplist ID")
+		return
+	}
+
+	userIDValue := r.Context().Value(userIDKey)
+	userID, ok := userIDValue.(int)
+	if !ok {
+		respondWithError(w, http.StatusBadRequest, "Invalid user ID type")
+		return
+	}
+
+	dbToplist, err := cfg.DB.GetToplist(toplistID)
+	if err != nil {
+		if errors.Is(err, database.ErrNotExist) {
+			respondWithError(w, http.StatusNotFound, "Toplist does not exist")
+			return
+		}
+		fmt.Println(err)
+		respondWithError(w, http.StatusInternalServerError, "Could not find toplist")
+		return
+	}
+
+	if dbToplist.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized to remove toplist")
 		return
 	}
 
