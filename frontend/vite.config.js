49bcfb33ca7dca71c767696edc71370873d56767
Emil Malmsten <emil_malmsten@hotmail.com>
2023-09-25 17:14:35 +0700

    make frontend env vars work in docker properly

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,31 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
-import dotenv from "dotenv";
 
-export default defineConfig(({ mode }) => {
-    const envFile =
-        mode === "development" ? ".env.development" : ".env.production";
-    dotenv.config({ path: envFile });
-    return {
-        plugins: [react()],
-        build: {
-            outDir: "build",
-            assetsDir: "assets",
-            emptyOutDir: true,
-        },
-        resolve: {
-            alias: {
-                "@": path.resolve(__dirname, "./src"),
-            },
-        },
-        server: {
-            proxy: {
-                "/api": {
-                    target: process.env.VITE_API_URL,
-                    changeOrigin: true,
-                },
-            },
-        },
-    };
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
 });
