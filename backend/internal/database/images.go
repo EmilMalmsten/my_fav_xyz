3f516f2a0a71c82de6a02471bb4e3df856a87ea4
Emil Malmsten <emil_malmsten@hotmail.com>
2023-10-12 16:23:37 +0700

    jpg only accepted img type

--- backend/internal/database/images.go
+++ backend/internal/database/images.go
@@ -64,10 +64,6 @@ func getExtensionFromMimeType(mimeType string) string {
 	switch mimeType {
 	case "image/jpeg":
 		return ".jpg"
-	case "image/png":
-		return ".png"
-	case "image/gif":
-		return ".gif"
 	default:
 		return ""
 	}
